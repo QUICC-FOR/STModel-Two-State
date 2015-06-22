80f991ddbd3fa76f21802b13d3537b898fd5b3cc
Matt Talluto <mtalluto@gmail.com>
2015-06-22 13:55:51 +0200

    Made a few revisions to speed up the SDM

--- scr/2_fit_sdm.r
+++ scr/2_fit_sdm.r
@@ -4,6 +4,7 @@ library(pROC)
 library(raster)
 library(rgdal)
 
+print("Setting up variables and reading files")
 annealFraction = 0.5
 
 speciesList = readRDS('dat/speciesList.rds')
@@ -15,8 +16,19 @@ ocean = spTransform(ocean, stmMapProjection)
 
 climDat = readRDS("dat/plotClimate_scaled.rds")
 
+
+# subset the data for the SDM
+# select only a single row for each plot (to avoid too much spatial duplication)
+# note that all species get the same subset (will be applied during the species loop)
+rows = sapply(unique(sdmDat$plot_id), function(i) {
+	candidates = which(sdmDat$plot_id == i)
+	if(length(candidates) == 1) candidates else sample(candidates, 1)
+	})
+
+
 for(spName in speciesList)
 {
+	print(paste("Starting species", spName))
 	baseDir = file.path('species', spName)
 
 	spPath = file.path(baseDir, 'dat', paste(spName, 'presence.rds', sep='_'))
@@ -24,31 +36,29 @@ for(spName in speciesList)
 	colnames(spData)[3] = 'presence'
 	sdmDat = merge(spData, climDat)
 
-	# subset the data for the SDM
-	# select only a single row for each plot (to avoid too much spatial duplication)
-	# drop the plot ID and year measured columns also
-	rows = sapply(unique(sdmDat$plot_id), function(i) {
-		candidates = which(sdmDat$plot_id == i)
-		if(length(candidates) == 1) candidates else sample(candidates, 1)
-		})
+	# apply the subset, and drop the plot ID and year measured columns also
 	sdmDat.subset = sdmDat[rows,-c(1,2)]
 	sdmDat.unsubset = sdmDat[-rows,-c(1,2)]
 
+	print("  Fitting RF model")
 	rf.mod = randomForest(as.factor(presence) ~ . , data = sdmDat.subset, ntree = 500)
 	saveRDS(rf.mod, file.path(baseDir, 'res', paste(spName, 'sdm.rds', sep='_'))
 
 	# see how well the rf fits
+	print("  Computing ROC")
 	ubsubset.predictions = predict(rf.mod, newdata = sdmDat.unsubset, type='prob')[,2]
 	rf.roc = roc(response=sdmDat.unsubset$presence, predictor=ubsubset.predictions)
 	cat("Area under the ROC curve: ", rf.roc$auc, "\n", 
 			file=file.path(baseDir, 'res', 'sdm_roc.txt'))
 
 	# project the SDM prob of presence to the transition observations
+	print("  Projecting SDM to transitions")
 	transitionData = readRDS(file.path(baseDir, 'dat', paste(spName, 'transitions.rds', sep='_')))
 	stmData = merge(transitionData, transitionClimate)
 	stmData$prevalence = predict(rf.mod, newdata = stmData, type='prob')[,2]
 	
 	# project SDM onto the climate grid
+	print("  Projecting SDM geographically")
 	climGrid = readRDS('dat/climateGrid_scaled.rds')
 	sdmProjection = data.frame(lon=climGrid$lon, lat=climGrid$lat, 
 			sdm=predict(rf.mod, newdata=climGrid, type='prob')[,2])
@@ -64,6 +74,7 @@ for(spName in speciesList)
 	
 	# diagnostic plots
 	# response curve
+	print("  Producing SDM plots")
 	pdf(file=file.path(baseDir, 'img', 'sdm_response.pdf'), width=10,height=7)
 	par(mfrow=c(2,3), bty='n', oma=c(0,0,2,0), mar=c(4.5,4.5,0.5,0.5))
 	xx = matrix(0, nrow=1000, ncol=6)
