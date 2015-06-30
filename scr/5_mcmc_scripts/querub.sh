6b2221836f932958d974dde1e58aab8e8a32fddb
Matt Talluto <mtalluto@gmail.com>
2015-06-30 12:54:34 +0200

    Fixed a dumb but harmless mistake in the mcmc scripts

--- scr/5_mcmc_scripts/querub.sh
+++ scr/5_mcmc_scripts/querub.sh
@@ -15,7 +15,6 @@ DIR=~/STModel-Two-State/species/$SPECIES
 
 cd $DIR; mkdir -p res/mcmc1 res/mcmc2 res/mcmc3
 
-mkdir $DIR/res/mcmc
 cd $DIR; $SRC/stm2_mcmc -p dat/mcmc_inits1.txt -t dat/mcmc_data.txt -o res/mcmc1 -n 1 -b 0 -i 20000 -c 40 -l 5 -v 2 2>mcmc_log1.txt &
 cd $DIR; $SRC/stm2_mcmc -p dat/mcmc_inits2.txt -t dat/mcmc_data.txt -o res/mcmc2 -n 1 -b 0 -i 20000 -c 40 -l 5 -v 2 2>mcmc_log2.txt &
 cd $DIR; $SRC/stm2_mcmc -p dat/mcmc_inits3.txt -t dat/mcmc_data.txt -o res/mcmc3 -n 1 -b 0 -i 20000 -c 40 -l 5 -v 2 2>mcmc_log3.txt &
