8a0b737400eac0d19c0782afdbd5f0651b3a80c3
Matt Talluto <mtalluto@gmail.com>
2015-07-02 17:14:22 +0200

    Updated qsub scripts for 4 more species

--- scr/5a_find_starting_vals.r
+++ scr/5a_find_starting_vals.r
@@ -2,7 +2,7 @@ library(coda)
 
 # temporary
 setwd("~/Dropbox/work/projects/STModel-Two-State_git/")
-spName = '32931-FRA-AME'
+spName = '183295-PIC-GLA'
 
 p1.raw = read.csv(file.path('species', spName, 'res', 'mcmc1', 'posterior.csv'))
 posterior = mcmc(p1.raw)
@@ -20,7 +20,11 @@ maxsamp = posterior[posterior[,maxpar] == min(posterior[,maxpar]),]
 findsum = function(x, fun)
 {
 	if(length(dim(x)) == 2)
-		return(x[which(apply(x, 1, sum) == fun(apply(x, 1, sum))),])
+	{
+		res = x[which(apply(x, 1, sum) == fun(apply(x, 1, sum))),]
+		if(is.matrix(res)) res = res[1,]
+		return(res)
+	}
 	if(is.vector(x))
 		return(x)
 	stop("Need a vector or matrix-like object")
@@ -29,7 +33,8 @@ findsum = function(x, fun)
 
 initMax = findsum(maxsamp, max)
 initMin = findsum(minsamp, min)
-plot(initMax, col='blue', ylim=c(min(initMin), max(initMax)), pch=16)
+ylim=c(min(c(initMin, initMax)), max(c(initMin, initMax)))
+plot(initMax, col='blue', ylim=ylim, pch=16)
 points(initMin, col='red', pch=16)
 abline(h=0)
 
