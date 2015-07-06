9f30b10ee8230c2703a635a3e07866eca58efc2c
Matt Talluto <mtalluto@gmail.com>
2015-07-06 16:08:12 +0200

    Added resume scripts for querub

--- scr/5_mcmc_scripts/querub2.sh
+++ scr/5_mcmc_scripts/querub2.sh
@@ -4,15 +4,16 @@
 #PBS -l nodes=1:ppn=40
 #PBS -r n
 #PBS -N querub2
-SPECIES=19408-QUE-RUB
 
 module load gcc/4.9.2
 module load openmpi/1.8.3
 
 # written for version 1.2
 SRC=~/STModel-MCMC/bin
-DIR=~/STModel-Two-State/species/$SPECIES
 
-cd $DIR; $SRC/stm2_mcmc -p dat/mcmc_inits2.txt -t dat/mcmc_data.txt -o res/mcmc2 -n 1 -b 0 -i 20000 -c 40 -l 5 -v 2 2>mcmc_log2.txt &
+
+SPECIES=19408-QUE-RUB
+DIR=~/STModel-Two-State/species/$SPECIES
+cd $DIR; $SRC/stm2_mcmc -r res/mcmc2/resumeData.txt -t dat/mcmc_data.txt -i 20000 2>mcmc_log2.txt &
 
 wait
