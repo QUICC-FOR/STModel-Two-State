daa0ef8ccc917de2c620209e3a035ffaea1b0304
Matt Talluto <mtalluto@gmail.com>
2015-10-16 12:11:16 +0200

    Folded both mcmc scripts together

--- scr/4_run_mcmc.sh
+++ scr/4_run_mcmc.sh
@@ -2,3 +2,8 @@
 for scr in scr/mcmc/*; do
 	qsub $scr
 done
+
+# run scripts for intercept only models
+for scr in scr/mcmc/int/*; do
+	qsub $scr
+done
--- scr/5_mcmc_intercept.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for dir in res/mcmc/*; do
-	mkdir -p "$dir"/i0 "$dir"/ia "$dir"/ig	
-done
-
-for scr in scr/mcmc/int/*; do
-	qsub $scr
-done
