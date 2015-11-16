c6fc8f69b6754761ff2ffbeb43ab571b6500559a
Matt Talluto <mtalluto@gmail.com>
2015-11-16 14:55:43 +0100

    Updated qsub scripts to submit one job per model for better output interpretation

--- scr/evaluation/do_map_eval.sh
+++ scr/evaluation/do_map_eval.sh
@@ -4,11 +4,5 @@
 #PBS -l nodes=1:ppn=40
 #PBS -r n
 #PBS -N mapeval
-
 DIR=~/STModel-Two-State
-MODELS=(0 a g i0 ia ig)
-
-cd $DIR; ./scr/evaluation/map_eval.r $SP 0 40 1000
-cd $DIR; ./scr/evaluation/map_eval.r $SP g 40 1000
-cd $DIR; ./scr/evaluation/map_eval.r $SP i0 40 1000
-cd $DIR; ./scr/evaluation/map_eval.r $SP ig 40 1000
+cd $DIR; ./scr/evaluation/map_eval.r $SP $MOD 40 1000
--- scr/evaluation/submit_map_eval.sh
+++ scr/evaluation/submit_map_eval.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 SPECIES=(18032-ABI-BAL 19049-ULM-AME 19290-QUE-ALB 19408-QUE-RUB 19481-BET-ALL 19489-BET-PAP 28728-ACE-RUB 28731-ACE-SAC 183302-PIC-MAR 195773-POP-TRE)
+MODELS=(0 a g i0 ia ig)
 DIR=~/STModel-Two-State
 for sp in "${SPECIES[@]}"
 do
-	qsub -vSP=$sp,MOD=$mod $DIR/scr/evaluation/do_map_eval.sh
+	for mod in "${MODELS[@]}"
+	do
+		qsub -vSP=$sp,MOD=$mod $DIR/scr/evaluation/do_map_eval.sh
+	done
 done
