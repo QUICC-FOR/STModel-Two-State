0a70e6f0b4b27f7add7b93579f7b3f321f3676c1
Matt Talluto <mtalluto@gmail.com>
2015-11-16 13:18:43 +0100

    Added qsub scripts for evaluation

---
+++ do_map_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#PBS -q default
+#PBS -l walltime=72:00:00
+#PBS -l nodes=1:ppn=40
+#PBS -r n
+#PBS -N mapeval
+
+DIR=~/STModel-Two-State
+MODELS=(0 a g i0 ia ig)
+
+cd $DIR; ./scr/map_eval.r $SP 0 40 1000
+cd $DIR; ./scr/map_eval.r $SP g 40 1000
+cd $DIR; ./scr/map_eval.r $SP i0 40 1000
+cd $DIR; ./scr/map_eval.r $SP ig 40 1000
---
+++ submit_map_eval.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SPECIES=(18032-ABI-BAL 19049-ULM-AME 19290-QUE-ALB 19408-QUE-RUB 19481-BET-ALL 19489-BET-PAP 28728-ACE-RUB 28731-ACE-SAC 183302-PIC-MAR 195773-POP-TRE)
+DIR=~/STModel-Two-State
+for sp in "${SPECIES[@]}"
+do
+	qsub -vSP=$sp,MOD=$mod $DIR/scr/do_ce_eval.sh
+done
