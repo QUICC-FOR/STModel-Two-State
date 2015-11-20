9948666c753d27928b6ad6d670419428657c6e1c
Matt Talluto <mtalluto@gmail.com>
2015-11-20 14:01:18 +0100

    Second try at posterior processing

---
+++ scr/post_processing/do_c_grid_preds.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#PBS -q default
+#PBS -l walltime=24:00:00
+#PBS -l nodes=10:ppn=40
+#PBS -r n
+#PBS -N grid_preds
+SPECIES=(18032-ABI-BAL 19049-ULM-AME 19290-QUE-ALB 19408-QUE-RUB 19481-BET-ALL 19489-BET-PAP 28728-ACE-RUB 28731-ACE-SAC 183302-PIC-MAR 195773-POP-TRE)
+DIR=~/STModel-Two-State
+for SP in "${SPECIES[@]}"
+do
+    cd $DIR; ./scr/post_processing/c_compute_grid_preds.r $SP 40 &
+done
+wait
