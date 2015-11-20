e8660f271f5c6c48c1fd5ba92255d5f45fd52342
Matt Talluto <mtalluto@gmail.com>
2015-11-20 13:03:02 +0100

    Updated scripts for posterior processing

---
+++ scr/post_processing/c_compute_grid_preds.r
@@ -0,0 +1,75 @@
+#!/usr/bin/env Rscript
+
+## depends:
+##    res/posterior/*
+
+## makes:
+##    res/posteriorGrid/*
+
+library(coda)
+library(doParallel)
+numCores=detectCores() - 2
+
+speciesList = readRDS('dat/speciesList.rds')
+models = c('0', 'i0', 'g', 'ig')
+speciesInfo = read.csv('dat/speciesInfo.csv')
+source('scr/stm_functions.r')
+
+
+arg = commandArgs(TRUE)
+if(length(arg) > 0)
+{
+	sps = which(speciesList %in% arg)
+	if(length(sps) == 0) 
+	{
+		warning("No species specified on command line; falling back to default")
+	} else 
+		speciesList = speciesList[which(speciesList %in% arg)]
+	corTest = as.numeric(arg[length(arg)])
+	if(!is.na(corTest)) numCores = corTest
+}
+registerDoParallel(cores=numCores)
+
+if(length(speciesList) == 0) stop("Error: no species specified")
+cat("Will process posteriors for:\n")
+for(sp in speciesList) cat("  ", sp, "\n")
+climScale = readRDS('dat/climate_scaling.rds')
+suppressWarnings(dir.create(file.path('res', 'posteriorGrid'), recursive=TRUE))
+
+climGrid = readRDS(file.path('dat', 'climateGrid_scaled.rds'))
+gr_env1 = climGrid$annual_mean_temp
+gr_env2 = climGrid$tot_annual_pp
+
+
+
+# the posterior columns containing extinction and colonization parameters
+e_pars = function(pars)	if(length(pars) > 2) pars[6:10] else pars[2]
+c_pars = function(pars)	if(length(pars) > 2) pars[1:5] else pars[1]
+
+
+for(spName in speciesList)
+{
+
+	posteriorFname = file.path('res', 'posterior', paste0(spName, '_posterior.rds'))
+	posterior = readRDS(posteriorFname)
+	info = speciesInfo[speciesInfo$spName == spName,]
+	calibDat = readRDS(file.path('dat', 'stm_calib', paste0(spName,'stm_calib.rds')))
+
+	for(mod in models)
+	{
+
+		curPosterior = posterior[[mod]]
+
+		grPredict_e = foreach(pars = iter(curPosterior, by='row'), .combine=rbind) %dopar%
+			predict.stm_point(e_pars(pars), gr_env1, gr_env2)
+		grPredict_c = foreach(pars = iter(curPosterior, by='row'), .combine=rbind) %dopar%
+			predict.stm_point(c_pars(pars), gr_env1, gr_env2)
+
+		grLambda = grPredict_c - grPredict_e
+		grPres = (grLambda > 0) * 1
+
+		saveRDS(list(coordinates = climGrid[,c('lon', 'lat')], E = grPredict_e, 
+				C = grPredict_c, lambda = grLambda, stmPres = grPres), 
+				file.path('res','posteriorGrid',paste0(spName,'_', mod, '_posteriorGrid.rds')))
+	}
+}
---
+++ scr/post_processing/submit_bc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SPECIES=(18032-ABI-BAL 19049-ULM-AME 19290-QUE-ALB 19408-QUE-RUB 19481-BET-ALL 19489-BET-PAP 28728-ACE-RUB 28731-ACE-SAC 183302-PIC-MAR 195773-POP-TRE)
+MODELS=(0 g i0 ig)
+DIR=~/STModel-Two-State
+for sp in "${SPECIES[@]}"
+do
+	for mod in "${MODELS[@]}"
+	do
+		qsub -vSP=$sp,MOD=$mod $DIR/scr/evaluation/do_bc.sh
+	done
+done
